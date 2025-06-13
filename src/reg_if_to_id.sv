b5f1a1e0d93a0e10ba3088409bb6e99c5d6d8121
Meowcaroni <sebastiancandelaria@live.com>
2025-06-13 10:13:56 -0400

    Prototype decode stage

--- src/reg_if_to_id.sv
+++ src/reg_if_to_id.sv
@@ -9,7 +9,8 @@ module reg_if_to_id #(ADDR_WIDTH = 64, INST_WIDTH = 32) (
     output wire                  inst_buffer_full,
     output wire [ADDR_WIDTH-1:0] d_pc4,
     output wire [ADDR_WIDTH-1:0] d_pc,
-    output wire [INST_WIDTH-1:0] d_inst
+    output wire [INST_WIDTH-1:0] d_inst,
+    output wire [INST_WIDTH-1:0] d_inst_next
 );
 
     reg [ADDR_WIDTH-1:0] pc4_reg;
@@ -38,8 +39,9 @@ module reg_if_to_id #(ADDR_WIDTH = 64, INST_WIDTH = 32) (
                               .write_en(~stall),
                               .data_in(inst),
                               .data_out(d_inst),
+                              .data_next(d_inst_next),
                               .is_empty(inst_buffer_empty),
                               .is_full(inst_buffer_full)
-                          );
+                             );
 
 endmodule
---
+++ src/register_bank_gpu.sv
@@ -0,0 +1,25 @@
+module register_bank_gpu #(REG_NUM = 32, DATA_WIDTH = 64)(
+    input  wire                       clk,
+    input  wire                       reset,
+    input  wire [$clog2(REG_NUM)-1:0] write_addr,
+    input  wire [DATA_WIDTH-1:0]      data_in,
+    input  wire                       write_en,
+    input  wire [$clog2(REG_NUM)-1:0] read_addr,
+    output wire [DATA_WIDTH-1:0]      data_out
+);
+    reg [DATA_WIDTH-1:0] registers [0:REG_NUM-1];
+    
+    // Reading data combinationally
+    assign data_out = registers[read_addr];
+
+    // Writing data in clock sequence
+    always_ff @(negedge clk) {
+        if (reset) begin
+            registers <= '0;
+        end
+        else if (write_en && (write_addr != 0)) begin
+            registers[write_addr] <= data_in;
+        end
+    }
+
+endmodule
